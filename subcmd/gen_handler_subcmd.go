ead78548fdc21fa96dec7a977c4ff1d2e7b0769f
ringbrew <ringxu666@163.com>
2022-10-09 00:40:31 +0800

    opt: handler gen

--- subcmd/gen_handler_subcmd.go
+++ subcmd/gen_handler_subcmd.go
@@ -28,14 +28,17 @@ func (sc *GenHandlerSubCmd) Process() error {
 func NewHandlerCommand() *cobra.Command {
 	return &cobra.Command{
 		Use:     "handler",
-		Short:   "generator for handler code. usage: gsv gen handler {domain} {subdomain}",
+		Short:   "generator for handler code. usage: gsv gen handler {domain} optional:{subdomain}",
 		Example: "gsv gen http demo example",
 		Run: func(cmd *cobra.Command, args []string) {
-			if len(args) != 2 {
+			if len(args) != 1 && len(args) != 2 {
 				log.Fatal(errors.New("invalid input").Error())
 			}
 			domain := args[0]
-			subdomain := args[1]
+			subdomain := domain
+			if len(args) == 2 {
+				subdomain = args[1]
+			}
 
 			log.Printf("gen handler command is running domain[%s]\n", domain)
 
