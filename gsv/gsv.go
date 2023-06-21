94247d2207cb04a9633c694336f1c51196ea8e00
ringbrew <ringxu666@163.com>
2023-06-21 14:43:50 +0800

    feat: init gen proto msg command

--- gsv/gsv.go
+++ gsv/gsv.go
@@ -24,6 +24,7 @@ func main() {
 	genCmd.AddCommand(subcmd.NewDomainCommand())
 	genCmd.AddCommand(subcmd.NewHttpCommand())
 	genCmd.AddCommand(subcmd.NewHandlerCommand())
+	genCmd.AddCommand(subcmd.NewGenProtoSubCmd())
 
 	rootCmd.AddCommand(genCmd)
 
